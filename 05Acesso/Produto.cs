9733868cf273555acad41139a099eda610839b70
Luís Fernandes <hsantos.luis@outlook.com>
2022-01-09 19:43:25 -0300

    Aula05 sobre modificadores de acesso

---
+++ 05Acesso/Produto.cs
@@ -0,0 +1,15 @@
+namespace _05Acesso
+{
+    public class Produto
+    {
+        /* 
+            Modificadores de acesso:
+            Public      -> Atributos e métodos visíveis em qualuqer classe
+            Private     -> Atributos e métodos visíveis apenas na classe onde são criados
+            Protected   -> Atributos e métodos visíveis em classes onde são criados ou herdados
+        */
+
+        public string nome;
+        private double valor;
+    }
+}
