ef3e6ea00cbab01b5790176f73c6e10e1160fce3
Luís Fernandes <hsantos.luis@outlook.com>
2022-01-11 20:56:13 -0300

    Aula08: Atributos GET e SET, acessando atributos privados

---
+++ 08GetSet/Pessoa.cs
@@ -0,0 +1,15 @@
+namespace _08GetSet
+{
+    public class Pessoa
+    {
+        //atributo
+        private string nome;
+
+        //get e set
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+    }
+}
