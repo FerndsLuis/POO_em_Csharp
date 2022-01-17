c5685dfd1bb59ddd917cc787922e55b9c0a3935a
Luís Fernandes <hsantos.luis@outlook.com>
2022-01-17 20:55:46 -0300

    Aula12: Atributos e métodos estáticos

---
+++ 12Estatico/Exemplo.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace _12Estatico
+{
+    public class Exemplo
+    {
+        //atributo estático
+        public static string nome = "LUÍS";
+
+        //método estático
+        public static void soma(int n1, int n2)
+        {
+            Console.WriteLine(n1 + n2);
+        }
+    }
+}
---
+++ 12Estatico/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _12Estatico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exemplo.soma(5, 9);
+
+            Console.WriteLine(Exemplo.nome);
+        }
+    }
+}
